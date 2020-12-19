c5f1ecf5c3b3c66cfd46ca166b90153adcd57ce8
NicolJamie <jamie@digidrip.co.uk>
2020-12-19 15:19:55 +0000

    feat(invoice): Creating ability to submit new invoices.

--- src/Invoices/Invoices.php
+++ src/Invoices/Invoices.php
@@ -3,30 +3,54 @@
 namespace NicolJamie\Sage\Invoices;
 
 use NicolJamie\Sage\Client;
-use NicolJamie\Sage\Transformer;
+use NicolJamie\Sage\Invoices\Data\Invoice;
 
 class Invoices extends Client
 {
-    use Transformer;
-
     /**
      * index
-     * @return Invoices
+     * @return object
      * @throws \Exception
      */
-    public function index(): Invoices
+    public function index(): object
     {
-        return $this->parse($this->base('GET', 'addresses'));
+        return Invoice::data($this->base('GET', 'sales_invoices'));
     }
 
-    public function show($key)
-    {}
-
-    public function store()
+    /**
+     * @param $key
+     * @return object
+     * @throws \Exception
+     */
+    public function show($key): object
     {
-        $this->base('POST', '', [
+        return Invoice::datum($this->base('GET', "sales_invoices/{$key}"));
+    }
 
-        ]);
+    /**
+     * store
+     * @param $data
+     * @param $key
+     * @return false|object
+     * @throws \Exception
+     */
+    public function store($data, $key)
+    {
+        if (!isset($data['lines'])) return false;
+
+        $body = [
+            'date' => $data['data'],
+        ];
+
+        $invoiceLines = [];
+        foreach ($data['lines'] as $line) {
+            $invoiceLines['ledger_account_id'] = $data['ledger_account_id'];
+        }
+
+        $body['contact_id'] = $key;
+        $body['invoice_lines'] = $invoiceLines;
+
+        return Invoice::datum($this->base('POST', 'sales_invoices', json_encode(['sales_invoice' => $body])));
     }
 
     public function update($key, $data)
