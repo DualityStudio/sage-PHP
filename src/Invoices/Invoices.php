cb2f8a453c71835760308b4df139218614f316c5
NicolJamie <jamie@digidrip.co.uk>
2020-12-20 13:06:42 +0000

    feat(invoice): Added invoice support.

--- src/Invoices/Invoices.php
+++ src/Invoices/Invoices.php
@@ -2,6 +2,7 @@
 
 namespace NicolJamie\Sage\Invoices;
 
+use Carbon\Carbon;
 use NicolJamie\Sage\Client;
 use NicolJamie\Sage\Invoices\Data\Invoice;
 
@@ -36,19 +37,13 @@ class Invoices extends Client
      */
     public function store($data, $key)
     {
-        if (!isset($data['lines'])) return false;
+        if (!isset($data['lines']) || !is_array($data['lines'])) return false;
 
         $body = [
-            'date' => $data['data'],
+            'date' => Carbon::parse($data['date'])->format('Y-m-d'),
+            'invoice_lines' => $data['lines'],
+            'contact_id' => $key
         ];
-
-        $invoiceLines = [];
-        foreach ($data['lines'] as $line) {
-            $invoiceLines['ledger_account_id'] = $data['ledger_account_id'];
-        }
-
-        $body['contact_id'] = $key;
-        $body['invoice_lines'] = $invoiceLines;
 
         return Invoice::datum($this->base('POST', 'sales_invoices', json_encode(['sales_invoice' => $body])));
     }
